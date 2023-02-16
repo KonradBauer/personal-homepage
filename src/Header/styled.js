f0fdf9edbd24d4c3b669111114f413460d8d64dc
Konrad Bauer <konradbauer94@gmail.com>
2023-02-16 22:39:16 +0100

    create Header container with photo and skills container

---
+++ src/Header/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  max-width: 1216px;
+  height: 384px;
+  margin: auto;
+  border: solid 1px black;
+  margin-top: 119px;
+`;
+
+export const Photo = styled.img`
+  position: absolute;
+  width: 398px;
+  height: 398px;
+  left: calc(50% - 398px / 2 - 416px);
+  top: 115px;
+  border-radius: 50%;
+`;
---
+++ src/SkillsContainer/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const SkillsContainer = styled.div`
+  max-width: 1216px;
+  height: 404px;
+  border: solid 1px black;
+  margin: auto;
+  position: absolute;
+  width: 1216px;
+  height: 404px;
+  left: calc(50% - 1216px / 2 + 2px);
+  top: 576px;
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+`;
