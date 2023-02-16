b45d7416c27257bfad78990fb50353f1e87a6ede
Konrad Bauer <konradbauer94@gmail.com>
2023-02-16 23:25:15 +0100

    added global styles to app

--- src/GlobalStyle/globalStyles.js
+++ src/GlobalStyle/globalStyles.js
@@ -1,8 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    mercury: "#E5E5E5",
+  },
+};
+
 export const GlobalStyle = createGlobalStyle`
   body {
-    background: #FFFFFF;
     font-family: 'Inter', sans-serif;
+    background: #FBFBFE;
   }
 `;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { GlobalStyle } from "./GlobalStyle/globalStyles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
