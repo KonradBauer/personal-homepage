3d67e36bf64cc0329217f6154fe966f99b43c563
Konrad Bauer <konradbauer94@gmail.com>
2023-02-16 22:39:34 +0100

    import containers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Header } from "./Header/styled";
+import { SkillsContainer } from "./SkillsContainer/styled";
+import image from "./images/konrad.png";
+import { Photo } from "./Header/styled";
+
 function App() {
-  return;
+  return (
+    <>
+      <Header>
+        <Photo src={image} alt="Konrad Bauer" />
+      </Header>
+      <SkillsContainer />
+    </>
+  );
 }
 
 export default App;
