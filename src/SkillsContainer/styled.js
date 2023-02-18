12c3dea69a10a97921b1cfc990995910de32f145
Konrad Bauer <konradbauer94@gmail.com>
2023-02-18 01:04:32 +0100

    fix skills container height

--- src/SkillsContainer/styled.js
+++ src/SkillsContainer/styled.js
@@ -2,11 +2,10 @@ import styled from "styled-components";
 
 export const SkillsContainer = styled.div`
   background: ${({ theme }) => theme.color.white};
+  margin-top: 576px;
   max-width: 1216px;
-  height: 404px;
+  height: 310px;
   margin: auto;
-  width: 1216px;
-  height: 404px;
   padding: 32px;
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
     0px 16px 58px rgba(9, 10, 51, 0.03);
@@ -27,6 +26,7 @@ export const SkillsContainerTitle = styled.text`
 
 export const Skills = styled.div`
   display: grid;
+  margin-top: 15px;
   grid-template-columns: 1fr 1fr 1fr;
   color: ${({ theme }) => theme.color.slateGray};
   font-style: normal;
