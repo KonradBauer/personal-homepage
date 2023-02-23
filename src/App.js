92dc075891d174230e8eabe1718d63f32204d015
Konrad Bauer <konradbauer94@gmail.com>
2023-02-23 17:25:49 +0100

    Social component moved to separate component, exported as Socials to App.js

--- src/App.js
+++ src/App.js
@@ -10,21 +10,12 @@ import {
 import image from "./common/images/konrad.png";
 import { ThemeProvider } from "styled-components";
 import { GlobalContainer, theme } from "./layoutData/GlobalStyle/globalStyles";
-import { ReactComponent as Hand } from "./common/svg/hand.svg";
-import { ReactComponent as GithubBlack } from "./common/svg/githubBlack.svg";
-import { ReactComponent as LinkedInBlack } from "./common/svg/linkedinBlack.svg";
-import {
-  Email,
-  Info,
-  LetsTalk,
-  Logos,
-  SocialContainer,
-} from "./layoutData/Social/styled";
 import { Button } from "./features/Buttons/index";
 import { SkillsList } from "./layoutData/SkillsContainer/index";
 import { NextToLearn } from "./layoutData/NextToLearnContaier/index";
 import { PortfolioHead } from "./layoutData/Portfolio/PortfolioHeader/index";
 import { PortfolioMain } from "./layoutData/Portfolio/PortfolioContent/index";
+import { Socials } from "./layoutData/Social/index";
 
 function App() {
   return (
@@ -45,24 +36,7 @@ function App() {
           <NextToLearn />
           <PortfolioHead />
           <PortfolioMain />
-          <SocialContainer>
-            <LetsTalk>Let’s talk!</LetsTalk>
-            <Email>konradbauer94@gmail.com</Email>
-            <Info>
-              I’m always open to new projects whenever I have the time. If you
-              have a website, dashboard or mobile app in mind and need some help
-              to make your ideas come to life, feel free to conatct me
-              <Hand />
-            </Info>
-            <Logos>
-              <a href="https://github.com/KonradBauer">
-                <GithubBlack />
-              </a>
-              <a href="https://linkedin.com/in/konrad-bauer">
-                <LinkedInBlack />
-              </a>
-            </Logos>
-          </SocialContainer>
+          <Socials />
         </GlobalContainer>
       </ThemeProvider>
     </>
---
+++ src/layoutData/Social/index.js
@@ -0,0 +1,27 @@
+import { SocialContainer, LetsTalk, Email, Info, Logos } from "./styled";
+import { ReactComponent as Hand } from "../../common/svg/hand.svg";
+import { ReactComponent as GithubBlack } from "../../common/svg/githubBlack.svg";
+import { ReactComponent as LinkedInBlack } from "../../common/svg/linkedinBlack.svg";
+
+export const Socials = () => {
+  return (
+    <SocialContainer>
+      <LetsTalk>Let’s talk!</LetsTalk>
+      <Email>konradbauer94@gmail.com</Email>
+      <Info>
+        I’m always open to new projects whenever I have the time. If you have a
+        website, dashboard or mobile app in mind and need some help to make your
+        ideas come to life, feel free to conatct me
+        <Hand />
+      </Info>
+      <Logos>
+        <a href="https://github.com/KonradBauer">
+          <GithubBlack />
+        </a>
+        <a href="https://linkedin.com/in/konrad-bauer">
+          <LinkedInBlack />
+        </a>
+      </Logos>
+    </SocialContainer>
+  );
+};
