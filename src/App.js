290a477c559fd91edf406186626fd46f559bbddb
Konrad Bauer <konradbauer94@gmail.com>
2023-02-24 00:36:03 +0100

    change viev to PortfolioMain

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <SkillsList />
           <NextToLearn />
           <PortfolioHead />
-          <ErrorStatus />
+          <PortfolioMain />
           <Socials />
         </GlobalContainer>
       </ThemeProvider>
