5bb077c83261890ca267169548b0ce4965ffb28a
Konrad Bauer <konradbauer94@gmail.com>
2023-02-23 01:35:33 +0100

    make skills list to look better

--- src/App.js
+++ src/App.js
@@ -58,31 +58,31 @@ function App() {
             <ContainerLine />
             <Skills>
               <List>
+                <ListItem>Teamwork</ListItem>
                 <ListItem>Semantic & accessible HTML</ListItem>
                 <ListItem>Responsive Web Design</ListItem>
-                <ListItem>Teamwork</ListItem>
-                <ListItem>Markdown</ListItem>
-                <ListItem>Immutability</ListItem>
                 <ListItem>CSS BEM convention</ListItem>
                 <ListItem>CSS Grid</ListItem>
                 <ListItem>CSS Flexbox</ListItem>
-              </List>
-              <List>
-                <ListItem>React Router</ListItem>
-                <ListItem>Redux-Saga</ListItem>
-                <ListItem>Redux (Toolkit)</ListItem>
-                <ListItem>React Hooks</ListItem>
-                <ListItem>Error handling</ListItem>
-                <ListItem>Working with API (fetch, axios)</ListItem>
                 <ListItem>JavaScript: ES6+</ListItem>
+                <ListItem>Promises, Async/Await</ListItem>
               </List>
               <List>
-                <ListItem>Promises, Async/Await</ListItem>
+                <ListItem>Immutability</ListItem>
+                <ListItem>Working with API (fetch, axios)</ListItem>
+                <ListItem>Error handling</ListItem>
+                <ListItem>React</ListItem>
+                <ListItem>React Hooks</ListItem>
+                <ListItem>Redux (Toolkit)</ListItem>
+                <ListItem>Redux-Saga</ListItem>
+                <ListItem>React Router</ListItem>
+              </List>
+              <List>
                 <ListItem>GitHub Pull Requests & Review</ListItem>
                 <ListItem>Trello</ListItem>
                 <ListItem>Scrum</ListItem>
                 <ListItem>NPM</ListItem>
-                <ListItem>React</ListItem>
+                <ListItem>Markdown</ListItem>
                 <ListItem>Git</ListItem>
               </List>
             </Skills>
