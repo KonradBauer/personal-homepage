e1bc1c1bc3d812a240c8d3b4f0c7b60dbdce8509
Konrad Bauer <konradbauer94@gmail.com>
2023-02-23 01:29:29 +0100

    added send mail feature

--- src/features/Buttons/index.js
+++ src/features/Buttons/index.js
@@ -2,8 +2,12 @@ import { ButtonHireMe } from "./styled";
 import { ReactComponent as ButtonElement } from "../../common/svg/buttonElement.svg";
 
 export const Button = () => {
+  const send = () => {
+    window.location.href = "mailto:konradbauer94@gmail.com";
+  };
+
   return (
-    <ButtonHireMe>
+    <ButtonHireMe onClick={send}>
       <ButtonElement />
       Hire Me
     </ButtonHireMe>
