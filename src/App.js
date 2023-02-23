b733da00f42f69ea4b26ec416d2f7327dd4b89f1
Konrad Bauer <konradbauer94@gmail.com>
2023-02-23 17:31:01 +0100

    Header moved to separate component, exported as HeaderContent to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
-import {
-  Header,
-  Photo,
-  ThisIs,
-  MyName,
-  AboutMe,
-} from "./layoutData/Header/styled";
 
-import image from "./common/images/konrad.png";
 import { ThemeProvider } from "styled-components";
 import { GlobalContainer, theme } from "./layoutData/GlobalStyle/globalStyles";
-import { Button } from "./features/Buttons/index";
+import { HeaderContent } from "./layoutData/Header/index";
 import { SkillsList } from "./layoutData/SkillsContainer/index";
 import { NextToLearn } from "./layoutData/NextToLearnContaier/index";
 import { PortfolioHead } from "./layoutData/Portfolio/PortfolioHeader/index";
@@ -22,16 +14,7 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <GlobalContainer>
-          <Header>
-            <Photo src={image} alt="Konrad Bauer" />
-            <ThisIs>This is</ThisIs>
-            <MyName>Konrad Bauer</MyName>
-            <AboutMe>
-              👨💻 I’m a passionate Frontend Developer in love with
-              React,currently looking for new job opportunities.
-            </AboutMe>
-            <Button />
-          </Header>
+          <HeaderContent />
           <SkillsList />
           <NextToLearn />
           <PortfolioHead />
---
+++ src/layoutData/Header/index.js
@@ -0,0 +1,18 @@
+import { Header, Photo, ThisIs, MyName, AboutMe } from "./styled";
+import { Button } from "../../features/Buttons";
+import image from "../../common/images/konrad.png";
+
+export const HeaderContent = () => {
+  return (
+    <Header>
+      <Photo src={image} alt="Konrad Bauer" />
+      <ThisIs>This is</ThisIs>
+      <MyName>Konrad Bauer</MyName>
+      <AboutMe>
+        👨💻 I’m a passionate Frontend Developer in love with React,currently
+        looking for new job opportunities.
+      </AboutMe>
+      <Button />
+    </Header>
+  );
+};
