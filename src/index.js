bef280caaf9c79a8207ecd8ed18530a0d2f2b5a0
Konrad Bauer <konradbauer94@gmail.com>
2023-02-26 14:53:31 +0100

    add Provider to index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from "./App";
 import { GlobalStyle } from "./common/GlobalStyle/globalStyles";
 import reportWebVitals from "./reportWebVitals";
 import "normalize.css";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
