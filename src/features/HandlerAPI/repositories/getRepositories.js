ebc790ae284472c764756fcd75c2d7fdd0256768
Konrad Bauer <konradbauer94@gmail.com>
2023-02-26 17:15:58 +0100

    fix api link

--- src/features/HandlerAPI/repositories/getRepositories.js
+++ src/features/HandlerAPI/repositories/getRepositories.js
@@ -3,5 +3,7 @@ import axios from "axios";
 const sourceURL = "https://api.github.com";
 
 export const getRepositories = (username) =>
-  axios.get(`${sourceURL}/users/$`).then((response) => response.data);
+  axios
+    .get(`${sourceURL}/users/${username}/repos`)
+    .then((response) => response.data);
 console.log(response.data);
