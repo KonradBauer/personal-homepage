0ea9cd82557ff0b863f07b6fa36f3adcb6ae8679
Konrad Bauer <konradbauer94@gmail.com>
2023-02-27 19:30:49 +0100

    add saga to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import projectsReducer from "./features/Projects/projectsSlice";
+import createSagaMiddleware from "redux-saga";
+import { watchLoadProjects } from "./features/Projects/projectsSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
   reducer: {
     projects: projectsReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(watchLoadProjects);
+
+export default store;
