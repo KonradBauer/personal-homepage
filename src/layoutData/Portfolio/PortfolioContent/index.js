74b9eb960104af1301826e9d8c11380f9c9595dd
Konrad Bauer <konradbauer94@gmail.com>
2023-02-27 19:35:16 +0100

    declared const userName

--- src/layoutData/Portfolio/PortfolioContent/index.js
+++ src/layoutData/Portfolio/PortfolioContent/index.js
@@ -1,6 +1,26 @@
 import { PortfolioContent, Tile } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  loadProjects,
+  loadProjectsError,
+  loadProjectsSuccess,
+  selectProjects,
+  selectProjectsStatus,
+} from "../../../features/Projects/projectsSlice";
+import { useEffect } from "react";
+import { getProjects } from "../../../features/Projects/getRepositories";
 
 export const PortfolioMain = () => {
+  const dispatch = useDispatch();
+
+  const getProjectsStatus = useSelector(selectProjectsStatus);
+  const projects = useSelector(selectProjects);
+  const userName = "KonradBauer";
+
+  useEffect(() => {
+    dispatch(loadProjects(userName));
+  }, []);
+
   return (
     <PortfolioContent>
       <Tile />
