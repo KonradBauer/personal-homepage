7b65d3fcf326e678f5d68658d2fa5cbe6ea91ef0
Konrad Bauer <konradbauer94@gmail.com>
2023-02-27 20:10:12 +0100

    portfolio loader switch added

---
+++ src/layoutData/Portfolio/portfolioLoader.js
@@ -0,0 +1,21 @@
+import { TileContent } from "./PortfolioContent/styled";
+import { LoadingStatus, ErrorStatus } from "../FetchStatus";
+
+export const PortfolioContent = ({ status, projects }) => {
+  switch (status) {
+    case "initial":
+      return null;
+
+    case "loading":
+      return <LoadingStatus />;
+
+    case "error":
+      return <ErrorStatus />;
+
+    case "success":
+      return <TileContent />;
+
+    default:
+      throw new Error(`incorrect status: ${status}`);
+  }
+};
