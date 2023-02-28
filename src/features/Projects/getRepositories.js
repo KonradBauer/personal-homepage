d8feb01842d8d9769f40c6409c011d5337f3dcc2
Konrad Bauer <konradbauer94@gmail.com>
2023-02-28 19:00:36 +0100

    files moved

--- src/features/Projects/getRepositories.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const githubAPIBaseURL = "https://api.github.com/";
-
-export const getProjects = (username) =>
-  axios
-    .get(`${githubAPIBaseURL}users/${username}/repos`)
-    .then((response) => console.log(response.data));
