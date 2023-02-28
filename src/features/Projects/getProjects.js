821df1ff171429f7efced3f3c3bf8a19b1e7e63c
Konrad Bauer <konradbauer94@gmail.com>
2023-02-28 18:58:56 +0100

    folder FetchStatus and getProjects moved to features, remove console.log from getProjects

---
+++ src/features/Projects/getProjects.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const githubAPIBaseURL = "https://api.github.com/";
+
+export const getProjects = (username) =>
+  axios
+    .get(`${githubAPIBaseURL}users/${username}/repos`)
+    .then((response) => response.data);
