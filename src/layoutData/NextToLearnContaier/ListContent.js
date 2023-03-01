664760115d8cf05c02cb6bb0b39902e1995e9bdf
Konrad Bauer <konradbauer94@gmail.com>
2023-03-01 22:05:02 +0100

    style List in ListNextToLearn

---
+++ src/layoutData/NextToLearnContaier/ListContent.js
@@ -0,0 +1,16 @@
+import { List, ListItem } from "../SkillsContainer/styled";
+
+export const ListNextToLearn = () => {
+  return (
+    <>
+      <List>
+        <ListItem>React Context</ListItem>
+        <ListItem>React Query</ListItem>
+        <ListItem>Cypress</ListItem>
+        <ListItem>Unit testing</ListItem>
+        <ListItem>TypeScript</ListItem>
+        <ListItem>Node.js</ListItem>
+      </List>
+    </>
+  );
+};
