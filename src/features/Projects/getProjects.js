40377e766da9fb8c669cb0b0de4b251e558ec7ad
Konrad Bauer <konradbauer94@gmail.com>
2023-03-02 21:01:52 +0100

    format api link

--- src/features/Projects/getProjects.js
+++ src/features/Projects/getProjects.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const githubAPIBaseURL = "https://api.github.com/";
+const githubAPIBaseURL = "https://api.github.com";
 
 export const getProjects = (username) =>
   axios
-    .get(`${githubAPIBaseURL}users/${username}/repos`)
+    .get(`${githubAPIBaseURL}/users/${username}/repos`)
     .then((response) => response.data);
