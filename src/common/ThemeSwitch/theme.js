1edcd1807cce1c7926aacf88872ed21aec97ce78
Konrad Bauer <konradbauer94@gmail.com>
2023-03-06 12:40:35 +0100

    create theme

--- src/common/ThemeSwitch/theme.js
+++ src/common/ThemeSwitch/theme.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { switchTheme, themeSliceState } from "./themeSlice";
+import { toggleTheme, selectThemeState } from "./themeSlice";
 import {
   StyledSun,
   Box,
@@ -11,11 +11,11 @@ import {
 
 export const ThemeSwitch = () => {
   const dispatch = useDispatch();
-  const themeDark = useSelector(themeSliceState);
+  const themeDark = useSelector(selectThemeState);
 
   return (
     <ThemeContainer>
-      <Button onClick={() => dispatch(switchTheme())}>
+      <Button onClick={() => dispatch(toggleTheme())}>
         <Text>dark mode {themeDark ? "on" : "off"}</Text>
         <Box>
           <IconWrapper move={themeDark}>
